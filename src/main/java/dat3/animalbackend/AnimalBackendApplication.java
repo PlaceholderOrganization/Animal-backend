65a253459af474a6071ac4c8ecc06309ced95f7f
Selmerr <molybdenum93@gmail.com>
2023-11-06 11:18:16 +0100

    test

---
+++ src/main/java/dat3/animalbackend/AnimalBackendApplication.java
@@ -0,0 +1,13 @@
+package dat3.animalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalBackendApplication.class, args);
+	}
+
+}
