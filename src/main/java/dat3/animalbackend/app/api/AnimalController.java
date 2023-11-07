3a2a3ef718428b5c0a52ab5519d3680566257b1a
Lotte Eliasson <lottefacereliasson@hotmail.com>
2023-11-07 10:43:10 +0100

    setup

--- src/main/java/dat3/animalbackend/app/api/AnimalController.java
+++ src/main/java/dat3/animalbackend/app/api/AnimalController.java
@@ -1,4 +1,25 @@
 package dat3.animalbackend.app.api;
 
+
+import dat3.animalbackend.app.dto.ChatResponse;
+import dat3.animalbackend.app.service.AnimalService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class AnimalController {
+
+    private AnimalService service;
+
+    public AnimalController(AnimalService service) {
+        this.service = service;
+    }
+
+    final static String SYSTEM_MESSAGE = "";
+
+    @GetMapping
+    public ChatResponse getAnswer(@RequestParam String answer){
+        return service.makeRequest(answer, SYSTEM_MESSAGE);
+    }
 }
--- src/main/java/dat3/animalbackend/app/dto/AnimalRequest.java
+++ src/main/java/dat3/animalbackend/app/dto/AnimalRequest.java
@@ -4,9 +4,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class AnimalRequest {
+
+    private String model;
+    private List<Message> messages = new ArrayList<>();
+    private double temperature;
+    private int max_tokens;
+    private double top_p;
+    private double frequency_penalty;
+    private double presence_penalty;
+
+    @Getter
+    @Setter
+    public static class Message {
+        private String role;
+        private String content;
+
+        public Message(String role, String content) {
+            this.role = role;
+            this.content = content;
+        }
+    }
 
 }
