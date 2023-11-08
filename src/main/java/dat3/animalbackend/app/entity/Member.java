d206658306e4a60e2dd86f619370a4d48e81fb3b
hannan <hannanxnoor@gmail.com>
2023-11-08 09:18:40 +0100

    animalsentity

---
+++ src/main/java/dat3/animalbackend/app/entity/Member.java
@@ -0,0 +1,26 @@
+package dat3.animalbackend.app.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name="memeber")
+public class Member{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "member_id")
+    private int member_Id;
+
+    @Column(name = "member_name") // Specify the column name for name
+    private String name;
+
+    @Column(name = "member_password") // Specify the column name for name
+    private String password;
+
+    @Column(name = "member_email") // Specify the column name for email
+    private String email;
+
+}
