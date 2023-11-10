f599d31adbf4b040954bb831ba67315c1a60d4c6
hannan <hannanxnoor@gmail.com>
2023-11-10 21:55:56 +0100

    ændringer login feature

--- src/main/java/dat3/animalbackend/app/repository/MemberRepository.java
+++ src/main/java/dat3/animalbackend/app/repository/MemberRepository.java
@@ -2,20 +2,27 @@ package dat3.animalbackend.app.repository;
 
 import dat3.animalbackend.app.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    Member findByNameAndPassword(String memberName, String memberPassword);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/dat3/animalbackend/app/service/MemberService.java
+++ src/main/java/dat3/animalbackend/app/service/MemberService.java
@@ -1,10 +1,13 @@
 package dat3.animalbackend.app.service;
 
 import dat3.animalbackend.app.dto.MemberRequest;
+import dat3.animalbackend.app.dto.MemberResponse;
 import dat3.animalbackend.app.entity.Member;
 import dat3.animalbackend.app.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
 
 @Service
 public class MemberService {
@@ -18,5 +21,21 @@ public class MemberService {
     }
 
 
+    public MemberResponse loginMember(MemberRequest memberRequest) throws AuthenticationException {
+        Member foundMember = memberRepository.findByNameAndPassword(
+                memberRequest.getMember_name(),
+                memberRequest.getMember_password()
+        );
+
+        if (foundMember != null) {
+            MemberResponse response = new MemberResponse();
+            response.setMember_name(foundMember.getName());
+            // You may include other relevant information in the response
+            response.setMessage("Login successful!");
+            return response;
+        }
+
+        throw new AuthenticationException("Invalid credentials");
+    }
 
 }
