dbeb30fedf7281d4390ceefdd86db89bb55aae6e
Selmerr <115707577+Selmerr@users.noreply.github.com>
2023-11-12 12:33:10 +0100

    Update settings.js

--- frontend/settings.js
+++ frontend/settings.js
@@ -1,5 +1,5 @@
-//export const API_URL = "http://openanimal.azurewebsites.net/api";
-export const API_URL = "http://localhost:8080/api";
+export const API_URL = "http://openanimal.azurewebsites.net/api";
+//export const API_URL = "http://localhost:8080/api";
 
 
 export const FETCH_NO_API_ERROR = "Could not fetch data from API. Make sure it is online and the endpoint exists.";
