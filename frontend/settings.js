5375937f9a8a3b877d6882f17974b3697110def9
jakobagger <113458342+jakobagger@users.noreply.github.com>
2023-11-12 02:03:00 +0100

    urls

--- frontend/settings.js
+++ frontend/settings.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://openanimal.azurewebsites.net/api";
-//export const API_URL = "https://localhost:8080/api/animal";
+//export const API_URL = "https://localhost:8080/api";
 
 export const FETCH_NO_API_ERROR = "Could not fetch data from API. Make sure it is online and the endpoint exists.";
